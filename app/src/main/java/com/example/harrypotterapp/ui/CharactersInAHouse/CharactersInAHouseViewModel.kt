73c759a2d7fa4a61d1662acb726ea79dfce151c1
Ana Paula Kelm Soares <ana.soares@seventh.com.br>
2025-08-30 22:26:42 -0300

    Cria um progress bar padrao

--- app/src/main/java/com/example/harrypotterapp/ui/CharactersInAHouse/CharactersInAHouseViewModel.kt
+++ app/src/main/java/com/example/harrypotterapp/ui/CharactersInAHouse/CharactersInAHouseViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.harrypotterapp.ui.CharactersInAHouse
 
-class CharactersInAHouseViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.harrypotterapp.data.model.Character
+import com.example.harrypotterapp.data.repository.CharRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class CharactersInAHouseViewModel (
+    private val repository: CharRepository
+
+) : ViewModel() {
+    suspend fun getCharactersByHouse(house: String): List<Character>? {
+        return try {
+            withContext(Dispatchers.IO) {
+                repository.getCharactersByHouse(house)
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
 }
