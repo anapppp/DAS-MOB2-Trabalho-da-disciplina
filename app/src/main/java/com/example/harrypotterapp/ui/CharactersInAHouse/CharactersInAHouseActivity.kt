80d9c492d234974c08a30d29502a42ea6f9bd183
Ana Paula Kelm Soares <ana.soares@seventh.com.br>
2025-08-30 22:46:33 -0300

    Faz a tela CharactersInAHouse

--- app/src/main/java/com/example/harrypotterapp/ui/CharactersInAHouse/CharactersInAHouseActivity.kt
+++ app/src/main/java/com/example/harrypotterapp/ui/CharactersInAHouse/CharactersInAHouseActivity.kt
@@ -1,28 +1,25 @@
 package com.example.harrypotterapp.ui.CharactersInAHouse
 
-import android.media.tv.TvView
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.harrypotterapp.R
 import com.example.harrypotterapp.data.model.Character
 import com.example.harrypotterapp.data.repository.CharRepository
-import com.example.harrypotterapp.ui.HogwartsStaff.HogwartsStaffViewModel
+import com.example.harrypotterapp.ui.hideLoading
+import com.example.harrypotterapp.ui.showLoading
 import kotlinx.coroutines.launch
 
 class CharactersInAHouseActivity : AppCompatActivity() {
 
     private lateinit var rgHouses: RadioGroup
-    private lateinit var tvCharList: TvView
+    private lateinit var tvCharList: TextView
     private lateinit var progressBar: ProgressBar
     private lateinit var viewModel: CharactersInAHouseViewModel
     private lateinit var repository: CharRepository
@@ -49,29 +46,28 @@ class CharactersInAHouseActivity : AppCompatActivity() {
             }
         }
     }
+
     private fun fetchStudentsByHouse(house: String) {
         lifecycleScope.launch {
             try {
-                showLoading()
-                val staff: List<Character>? = viewModel.getHogwartsStaff()
+                showLoading(progressBar, tvCharList)
+                val staff: List<Character>? = viewModel.getCharactersByHouse(house)
                 val names = staff?.joinToString(separator = "\n") { it.name ?: "" }
 
-                tvStaffList.text = names
+                tvCharList.text = names
 
 
             } catch (e: Exception) {
                 Toast.makeText(
-                    this@HogwartsStaffActivity,
+                    this@CharactersInAHouseActivity,
                     "Erro ao buscar professores",
                     Toast.LENGTH_SHORT
                 ).show()
                 Log.e("HogwartsStaffActivity", "Erro ao buscar professores", e)
             } finally {
-                hideLoading()
+                hideLoading(progressBar, tvCharList)
             }
         }
-
-
     }
 }
 
